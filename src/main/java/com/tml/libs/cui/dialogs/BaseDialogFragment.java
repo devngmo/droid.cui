50af22f209c1229f2663f822186aeaff9f0c98b7
devngmo <devngmo@gmail.com>
2017-04-11 18:45:09 +0700

    + add BaseDialogFragment - update dlg_fragment_okcancel.xml

---
+++ src/main/java/com/tml/libs/cui/dialogs/BaseDialogFragment.java
@@ -0,0 +1,28 @@
+package com.tml.libs.cui.dialogs;
+
+import android.app.Activity;
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by ManhLinh on 4/9/2017.
+ */
+
+public class BaseDialogFragment<T> extends DialogFragment {
+    private T mActivityInstance;
+
+    public final T getActivityInstance() {
+        return mActivityInstance;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        mActivityInstance = (T) activity;
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mActivityInstance = null;
+    }
+}
